16b24c31018232f4c34d8152f0a56e702336e70c
junedipasarib <89959703+junedipasarib@users.noreply.github.com>
2021-09-10 09:46:40 +0700

    Add files via upload

---
+++ deretbilangan.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char** argv) {
+	int panjang;
+	cout <<"1+1 ="<<2<<endl;
+	cout <<"2+3 ="<<5<< endl;
+	cout <<"5+8 ="<<13<< endl;
+	cout <<"hasil ="<<21<< endl;
+	return 0;
+}
